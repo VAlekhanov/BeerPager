1cf2632daf222bcf49ff3ce7fe6ba9db186d45de
Alehanov <bitvull@gmail.com>
2022-06-09 18:39:40 +0400

    фвфывфыв

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class App {
+    //https://alekseygulynin.ru/chat-na-java-klientskaya-chast/
+    //https://alekseygulynin.ru/chat-na-java-servernaya-chast/
     public static void main(String[] args) {
-        
+
     }
 }
